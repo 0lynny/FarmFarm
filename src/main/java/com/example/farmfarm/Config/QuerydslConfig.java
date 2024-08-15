6fbad919a58c416052d9c99261aed0c34188b8a0
Choi YoungLyn <joeuni0129@gmail.com>
2024-08-15 23:47:35 +0900

    chore: Querydsl Setting

---
+++ src/main/java/com/example/farmfarm/Config/QuerydslConfig.java
@@ -0,0 +1,21 @@
+package com.example.farmfarm.Config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(entityManager);
+        return jpaQueryFactory;
+    }
+}
